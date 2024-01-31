5fdc5c81d53effd536b268702d48505cad23adc3
poonam <poonam@gmail.com>
2024-01-31 15:25:22 +0530

    Added infrastructure of project

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -2,4 +2,12 @@ package pages;
 
 public class LoginPage {
 
+	
+	public void loginToApp() {
+		System.out.println("Login To Application");
+	}
+	
+	public void fetchTitle() {
+		System.out.println("Getting title of page");
+	}
 }
