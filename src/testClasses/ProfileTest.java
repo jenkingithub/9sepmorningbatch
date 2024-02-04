5dac7ae21669d444c0a3c85a2bca5e76251511ad
unknown <poonam@gmail.com>
2024-02-04 22:52:39 +0530

    added ProfileTest class

---
+++ src/testClasses/ProfileTest.java
@@ -0,0 +1,12 @@
+package testClasses;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	@Test
+	public void loginTest() {
+		
+		System.out.println("validating login page");
+	}
+}
